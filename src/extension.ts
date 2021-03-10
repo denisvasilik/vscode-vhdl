849cbe1e98a0164609826eba8a1bd0bf7d1ecaa5
Denis Vasilik <contact@denisvasilik.com>
2021-03-10 20:43:49 +0100

    Add debug server options

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,24 @@
 import * as path from 'path';
-import { 
-    workspace, 
-    Disposable, 
-    ExtensionContext 
-} from 'vscode'; 
-import { 
-    LanguageClient, 
-    LanguageClientOptions, 
-    ServerOptions 
+import * as net from 'net';
+import {
+    workspace,
+    Disposable,
+    ExtensionContext
+} from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+	StreamInfo
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
     const executablExt = process.platform == 'win32' ? '.bat' : '';
-	const executable = 'language-server' + executablExt;
+    const executable = 'language-server' + executablExt;
     const command = context.asAbsolutePath(path.join('resources', 'bin', executable));
     const serverOptions = { command };
-    
     const clientOptions: LanguageClientOptions = {
         documentSelector: [{ scheme: 'file', language: 'vhdl' }],
         synchronize: {
@@ -26,10 +27,24 @@ export function activate(context: ExtensionContext) {
         }
     }
 
-	client = new LanguageClient(
+    // Debug server options
+    // let connectionInfo = {
+    //     port: 5007
+    // };
+    // let serverOptions = () => {
+    //     // Connect to language server via socket
+    //     let socket = net.connect(connectionInfo);
+    //     let result: StreamInfo = {
+    //         writer: socket,
+    //         reader: socket
+    //     };
+    //     return Promise.resolve(result);
+    // };
+
+    client = new LanguageClient(
         'vhdl',
-        'VHDL', 
-        serverOptions, 
+        'VHDL',
+        serverOptions,
         clientOptions
     )
 
